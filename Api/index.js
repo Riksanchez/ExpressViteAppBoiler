6562b540a18e9cc163c194a6d7c2e546fa071de8
Erik <erik@eriks-mbp.mynetworksettings.com>
2024-05-31 18:16:53 -0400

    testing completeed for front end

--- Api/index.js
+++ Api/index.js
@@ -2,7 +2,9 @@ import express from "express";
 
 const app = express();
 app.use(express.json());
-
+app.get("/", (req,res) =>{
+    console.log(res.json("Hello Welome to this Splendid App You MotherFucker !"))
+})
 app.listen(5172,() =>{
     console.log("connected")
 });
--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -5,7 +5,7 @@ const App = () => {
 
   return (
     <>
-    
+      <div className='test'>Hi</div>
     </>
   )
 }
